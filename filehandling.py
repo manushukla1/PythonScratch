032534a22d786ca7495acb46d6cabecf81e242b1
Manu Shukla <manushukla64@gmail.com>
2025-05-03 19:03:43 +0530

    changes

--- filehandling.py
+++ filehandling.py
@@ -9,8 +9,7 @@ for line in lines:
     pi_string += line.strip()
 
 print(pi_string)'''
-
-
+"""
 path = Path('/Users/manushukla/Downloads/pi_digits.txt')
 contents = path.read_text()
 lines = contents.splitlines()
@@ -22,3 +21,29 @@ if birthday in pi_string:
     print ("Your birthday appears in the first million digits of pi!")
 else:
     print("Your birthday does not appear in the first million digits of pi.")
+"""
+
+file = open("/Users/manushukla/Downloads/pi_digits.txt", "w",)
+file.write("Manu,\n")
+file.write("Shukla\n")
+file.close()
+file = open("/Users/manushukla/Downloads/pi_digits.txt", "r")
+print(file.read())
+
+# ➕ If you want to add to the file (instead of overwriting), use 'a':
+file = open("/Users/manushukla/Downloads/pi_digits.txt", "a",)
+file.write("don")
+file = open("/Users/manushukla/Downloads/pi_digits.txt", "r")
+print(file.read())
+
+# Best Practice: Use with (auto closes file)
+#  .writelines() – What Is It?
+lines = ["Manu shukla,\n"
+         "pyhton coder"]
+
+file = open("/Users/manushukla/Downloads/pi_digits.txt", "r+",)
+file.writelines(lines)
+file = open("/Users/manushukla/Downloads/pi_digits.txt", "r")
+print(file.read())
+
+
